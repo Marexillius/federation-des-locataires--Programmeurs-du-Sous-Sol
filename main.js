3ac5f3d412ec349153625b54689e05edb7f64802
sideking <2234847@cmontmorency.qc.ca>
2024-09-19 14:19:19 -0400

    lien vrai site et close pop up button
    modification du js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+//jay
 const BurgerButton = document.querySelector("#Burger");
+const OriginalSiteButton = document.querySelector("#OriginalSite");
+const closePopUp = document.querySelector("#ClosePopUp");
+
+closePopUp.addEventListener("click", function() {
+    document.querySelector(".projet").style.display = "none";
+})
+
+OriginalSiteButton.addEventListener("click", function() {
+    window.open("https://www.flhlmq.com/fr", "_blank");
+})
