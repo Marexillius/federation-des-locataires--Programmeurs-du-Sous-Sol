1d1c8e7dfc0b286f77e08808991387e0f32ec8fa
Marexillius <alexandregervaisme@gmail.com>
2024-10-02 22:23:57 -0400

    ajouté le Javascript pour les services

--- main.js
+++ main.js
@@ -15,4 +15,21 @@ const hero2 = document.querySelector(".hero__nouvelles--2");
 
 hero2.addEventListener("click", function() {
     this.style.left = "100%"
-})
+});
+
+// #####################################
+//    S E R V I C E S   O F F E R T S
+// #####################################
+// Alexandre
+const servicesCard = document.querySelectorAll(".services__carte");
+
+servicesCard.forEach(card => {
+    card.addEventListener("click", function() {
+        servicesCard.forEach(element => {
+            if (element.classList.contains("services__carte--active")) {
+                element.classList.remove("services__carte--active");
+            } 
+        });
+        card.classList.add("services__carte--active");
+    })
+});
