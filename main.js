29a103f3f0146d9386dbdf1c1dfcf70e1806da53
sideking <2234847@cmontmorency.qc.ca>
2024-10-02 22:46:30 -0400

    centering

--- main.js
+++ main.js
@@ -28,12 +28,12 @@ for (let index = 0; index < heros.length; index++) {
             this.style.transform = "translateX(-100%)";
             
             
-            heros[1].style.transform = "translate(0, -103%)";
+            heros[1].style.transform = "translate(0, -102%)";
 
             heros[2].style.transitionDuration = "0s";
             heros[2].style.transform = "translate(100%, -206%)";
         } else if (this == heros[1]) {
-            this.style.transform = "translate(-100%, -103%)";
+            this.style.transform = "translate(-100%, -102%)";
             
 
             heros[2].style.transform = "translate(0, -206%)";
@@ -47,7 +47,7 @@ for (let index = 0; index < heros.length; index++) {
             heros[0].style.transform = "translate(0, 0%)";
             
             heros[1].style.transitionDuration = "0s";
-            heros[1].style.transform = "translate(100%, -103%)";
+            heros[1].style.transform = "translate(100%, -102%)";
         } 
         
  
