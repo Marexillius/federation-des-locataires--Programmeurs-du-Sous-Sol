d05cf844def0cb81746f6f65d4a900a3fa4a9ced
Marexillius <alexandregervaisme@gmail.com>
2024-12-02 14:26:21 -0500

    i tried and failed

--- wp-content/themes/FLHLMQ/main.js
+++ wp-content/themes/FLHLMQ/main.js
@@ -54,3 +54,29 @@ const swiperService = new Swiper('.service__swipper', {
       prevEl: '.swiper-button-prev',
     },
 });
+
+// Alexandre
+try {
+  const robertCard = document.querySelector(".modal__member--robert");
+  const elisabethCard = document.querySelector(".modal__member--elisabeth");
+  const patriciaCard = document.querySelector(".modal__member--patricia");
+  const anikCard = document.querySelector(".modal__member--anik");
+  const modal = document.querySelector(".modal");
+
+  robertCard.addEventListener("click", getModal("robert"));
+  elisabethCard.addEventListener("click", getModal("elisabeth"));
+  patriciaCard.addEventListener("click", getModal("patricia"));
+  anikCard.addEventListener("click", getModal("anik"));
+
+  function getModal(memberName) {
+    let person = document.querySelector(`.${memberName}`);
+    person.classList.remove("modal__hidden");
+    modal.style.position = "fixed";
+    modal.addEventListener("click", function(){
+      person.classList.add("modal__hidden");
+      modal.style.position = "unset";
+    })
+  }
+} catch(err) {
+  console.log(err);
+}
