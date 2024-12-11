34a6366df9c74e3b70ab3a90c2b040678e58a8d2
IanQc <ian.corbin.qc@gmail.com>
2024-12-10 21:55:39 -0500

    oui

--- wp-content/themes/FLHLMQ/news-hub.php
+++ wp-content/themes/FLHLMQ/news-hub.php
@@ -1,32 +1,70 @@
-<?php 
+<?php
+
 /**
  * 	Template Name: Nouvelles
- * Template Post Type: post, page, product
+ * Template Post Type: post, page, product, news
  * 	Identique à page, mais avec une barre latérale
  */
 
+
+ // Alexandre
 get_header(); // Affiche header.php
-
-if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
-	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
-	while ( have_posts() ) : the_post(); 
 ?>
+<section class="allNews">
+	<div class="savior">
+		<div class="savior__hero">
+			<div class="savior__icon">
+				<img src="<?php bloginfo('template_url'); ?>/images/newspaper_white.png">
+			</div>
+			<h2 class="savior__title">Nouvelles</h2>
+		</div>
+	</div>
 
-	<article>
-		<?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-			<h2>
-				<?php the_title(); // Titre de la page ?>
-			</h2>
-		<?php endif; ?>
-		
-		<?php the_content(); // Contenu principal de la page ?>
-	</article>
-<?php endwhile; // Fermeture de la boucle
+	<div>
+		<div class="allNews__filtrer">
+			<p class="allNews__filtrer__sort">Filtrer</p>
+			<select class="allNews__filtrer__selection" name="" id="filtre">
+				<option value="old">Ordre décroissant</option>
+				<option value="recent">Ordre croissant</option>
+			</select>
+		</div>
+		<div class="allNews__grid-container">
+			<div class="allNews__late">
+				<?php
+				// Check si y'a des posts
+				if (have_posts()) :
+					// Crée un tableau avec les nouvelles ordonnées
+					$nouvelle = array(
+						'orderby' => array(
+							// Ordonne le tableau par les nouvelles les plus récentes d'abord.
+							'date' => 'DESC',
+						),
+						// collecte les post types des nouvelles
+						'post_type' => 'nouvelle',
+					);
+					$article = new WP_Query($nouvelle);
+					while ($article->have_posts()) : $article->the_post();
+				?>
 
-else : // Si aucune page n'a été trouvée
-	get_template_part( 'partials/404' ); // Affiche partials/404.php
-endif;
-
-get_sidebar(); // Affiche le contenu de sidebar.php
-get_footer(); // Affiche footer.php 
+						<div class="allNews__Item">
+							<!-- Ajoute l'image de la nouvelle wordpress -->
+							<img class="allNews__img" src='<?php the_post_thumbnail_url()?>'>
+							<!-- Ajoute le titre de la nouvelle wordpress -->
+							<h2 class="allNews__titreCarte"><?php the_title()?></h2>
+							<!-- Ajoute la description preview de la nouvelle wordpress -->
+							<p class="allNews__paraCarte"><?php echo esc_html ( get_field( 'previewnouvelle' ) ); ?></p>
+						</div>
+					<?php endwhile ?>
+			</div>
+			<div class="allNews__more"> Plus de nouvelles </div>
+		</div>
+	</div>
+</section>
+<?php
+				else : // Si URL est invalide
+					// Montre la page 404
+					get_template_part('partials/404');
+				endif;
+				// Affiche le footer
+				get_footer();
 ?>
